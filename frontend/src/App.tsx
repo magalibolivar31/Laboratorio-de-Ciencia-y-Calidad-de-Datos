9edce63dc661ae3f987d165500ad975e88f4fdfe
Maria Florencia Rossi <florfacultad1060@gmail.com>
2026-06-13 13:20:11 -0300

    Actualización de identidad institucional UAI CAETI y diseño Home Glassmorphism

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,21 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Settings from './pages/Settings';
+import Exports from './pages/Exports';
+import SearchLaboratory from './pages/SearchLaboratory';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/settings" element={<Settings />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/exports" element={<Exports />} />
+        <Route path="/laboratory" element={<SearchLaboratory />} />
       </Routes>
     </Router>
   );
